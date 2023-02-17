e94d712bd4a27eec931eedb2222c2cca3beb0fc1
Isaac Trost <CMP_IsTrost@ects.org>
2023-02-16 23:34:15 -0500

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,206 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.titleBar = New System.Windows.Forms.MenuStrip()
+        Me.SettingsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.board = New System.Windows.Forms.FlowLayoutPanel()
+        Me.lblTurn = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lblXWins = New System.Windows.Forms.Label()
+        Me.lblYWins = New System.Windows.Forms.Label()
+        Me.btnMinimize = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.TwoPlayerToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.titleBar.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'titleBar
+        '
+        Me.titleBar.AutoSize = False
+        Me.titleBar.BackColor = System.Drawing.Color.FromArgb(CType(CType(20, Byte), Integer), CType(CType(20, Byte), Integer), CType(CType(20, Byte), Integer))
+        Me.titleBar.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SettingsToolStripMenuItem})
+        Me.titleBar.Location = New System.Drawing.Point(0, 0)
+        Me.titleBar.Name = "titleBar"
+        Me.titleBar.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional
+        Me.titleBar.Size = New System.Drawing.Size(345, 24)
+        Me.titleBar.TabIndex = 0
+        Me.titleBar.Text = "MenuStrip1"
+        '
+        'SettingsToolStripMenuItem
+        '
+        Me.SettingsToolStripMenuItem.BackColor = System.Drawing.Color.FromArgb(CType(CType(20, Byte), Integer), CType(CType(20, Byte), Integer), CType(CType(20, Byte), Integer))
+        Me.SettingsToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripMenuItem1, Me.TwoPlayerToolStripMenuItem})
+        Me.SettingsToolStripMenuItem.Font = New System.Drawing.Font("Microsoft PhagsPa", 9.0!, System.Drawing.FontStyle.Bold)
+        Me.SettingsToolStripMenuItem.ForeColor = System.Drawing.Color.White
+        Me.SettingsToolStripMenuItem.Name = "SettingsToolStripMenuItem"
+        Me.SettingsToolStripMenuItem.Size = New System.Drawing.Size(65, 20)
+        Me.SettingsToolStripMenuItem.Text = "Settings"
+        '
+        'ToolStripMenuItem1
+        '
+        Me.ToolStripMenuItem1.BackColor = System.Drawing.Color.FromArgb(CType(CType(31, Byte), Integer), CType(CType(31, Byte), Integer), CType(CType(31, Byte), Integer))
+        Me.ToolStripMenuItem1.Checked = True
+        Me.ToolStripMenuItem1.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.ToolStripMenuItem1.ForeColor = System.Drawing.Color.White
+        Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
+        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(148, 22)
+        Me.ToolStripMenuItem1.Text = "Two Player"
+        Me.ToolStripMenuItem1.TextDirection = System.Windows.Forms.ToolStripTextDirection.Horizontal
+        Me.ToolStripMenuItem1.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage
+        '
+        'board
+        '
+        Me.board.Location = New System.Drawing.Point(12, 74)
+        Me.board.Name = "board"
+        Me.board.Size = New System.Drawing.Size(321, 321)
+        Me.board.TabIndex = 3
+        '
+        'lblTurn
+        '
+        Me.lblTurn.Font = New System.Drawing.Font("Microsoft PhagsPa", 18.0!, System.Drawing.FontStyle.Bold)
+        Me.lblTurn.ForeColor = System.Drawing.Color.White
+        Me.lblTurn.Location = New System.Drawing.Point(76, 31)
+        Me.lblTurn.Name = "lblTurn"
+        Me.lblTurn.Size = New System.Drawing.Size(195, 37)
+        Me.lblTurn.TabIndex = 4
+        Me.lblTurn.Text = "__'s Turn"
+        Me.lblTurn.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label2
+        '
+        Me.Label2.Font = New System.Drawing.Font("Microsoft PhagsPa", 18.0!, System.Drawing.FontStyle.Bold)
+        Me.Label2.ForeColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(28, 413)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(104, 37)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "X Wins"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label3
+        '
+        Me.Label3.Font = New System.Drawing.Font("Microsoft PhagsPa", 18.0!, System.Drawing.FontStyle.Bold)
+        Me.Label3.ForeColor = System.Drawing.Color.White
+        Me.Label3.Location = New System.Drawing.Point(211, 414)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(104, 37)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Y Wins"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblXWins
+        '
+        Me.lblXWins.Font = New System.Drawing.Font("Microsoft PhagsPa", 18.0!, System.Drawing.FontStyle.Bold)
+        Me.lblXWins.ForeColor = System.Drawing.Color.White
+        Me.lblXWins.Location = New System.Drawing.Point(28, 450)
+        Me.lblXWins.Name = "lblXWins"
+        Me.lblXWins.Size = New System.Drawing.Size(104, 37)
+        Me.lblXWins.TabIndex = 7
+        Me.lblXWins.Text = "0"
+        Me.lblXWins.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblYWins
+        '
+        Me.lblYWins.Font = New System.Drawing.Font("Microsoft PhagsPa", 18.0!, System.Drawing.FontStyle.Bold)
+        Me.lblYWins.ForeColor = System.Drawing.Color.White
+        Me.lblYWins.Location = New System.Drawing.Point(211, 450)
+        Me.lblYWins.Name = "lblYWins"
+        Me.lblYWins.Size = New System.Drawing.Size(104, 37)
+        Me.lblYWins.TabIndex = 8
+        Me.lblYWins.Text = "0"
+        Me.lblYWins.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btnMinimize
+        '
+        Me.btnMinimize.BackgroundImage = CType(resources.GetObject("btnMinimize.BackgroundImage"), System.Drawing.Image)
+        Me.btnMinimize.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.btnMinimize.FlatAppearance.BorderSize = 0
+        Me.btnMinimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnMinimize.Location = New System.Drawing.Point(296, 0)
+        Me.btnMinimize.Name = "btnMinimize"
+        Me.btnMinimize.Size = New System.Drawing.Size(26, 24)
+        Me.btnMinimize.TabIndex = 2
+        Me.btnMinimize.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.BackgroundImage = CType(resources.GetObject("btnExit.BackgroundImage"), System.Drawing.Image)
+        Me.btnExit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.btnExit.FlatAppearance.BorderSize = 0
+        Me.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnExit.Location = New System.Drawing.Point(321, 0)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(24, 24)
+        Me.btnExit.TabIndex = 1
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'TwoPlayerToolStripMenuItem
+        '
+        Me.TwoPlayerToolStripMenuItem.BackColor = System.Drawing.Color.FromArgb(CType(CType(31, Byte), Integer), CType(CType(31, Byte), Integer), CType(CType(31, Byte), Integer))
+        Me.TwoPlayerToolStripMenuItem.ForeColor = System.Drawing.Color.White
+        Me.TwoPlayerToolStripMenuItem.Name = "TwoPlayerToolStripMenuItem"
+        Me.TwoPlayerToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.TwoPlayerToolStripMenuItem.Text = "Single Player"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(39, Byte), Integer), CType(CType(39, Byte), Integer), CType(CType(39, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(345, 501)
+        Me.Controls.Add(Me.lblYWins)
+        Me.Controls.Add(Me.lblXWins)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.lblTurn)
+        Me.Controls.Add(Me.board)
+        Me.Controls.Add(Me.btnMinimize)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.titleBar)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.MainMenuStrip = Me.titleBar
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.titleBar.ResumeLayout(False)
+        Me.titleBar.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents titleBar As MenuStrip
+    Friend WithEvents btnExit As Button
+    Friend WithEvents btnMinimize As Button
+    Friend WithEvents board As FlowLayoutPanel
+    Friend WithEvents lblTurn As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lblXWins As Label
+    Friend WithEvents lblYWins As Label
+    Friend WithEvents SettingsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents TwoPlayerToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ Form1.vb
@@ -0,0 +1,248 @@
+﻿Imports System.Runtime.InteropServices
+
+Public Class Form1
+
+
+#Region "TitleBar"
+    Public Const WM_NCLBUTTONDOWN As Integer = &HA1
+    Public Const HT_CAPTION As Integer = &H2
+
+    <DllImportAttribute("user32.dll")>
+    Public Shared Function SendMessage(ByVal hWnd As IntPtr, ByVal Msg As Integer, ByVal wParam As Integer, ByVal lParam As Integer) As Integer
+    End Function
+
+    <DllImportAttribute("user32.dll")>
+    Public Shared Function ReleaseCapture() As Boolean
+    End Function
+
+    Private Sub CustomTitle_MouseDown(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles titleBar.MouseDown, titleBar.MouseDown
+        If e.Button = Windows.Forms.MouseButtons.Left Then
+            ReleaseCapture()
+            SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0)
+        End If
+    End Sub
+
+    Private Sub btnMinimize_Click(sender As Object, e As EventArgs) Handles btnMinimize.Click
+        Me.WindowState = FormWindowState.Minimized
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+#Region "Exit Button"
+    Private Sub btnExitEnter(sender As Object, e As EventArgs) Handles btnExit.MouseEnter
+        btnExit.BackgroundImage = My.Resources.x_hover
+    End Sub
+    Private Sub btnExitExit(sender As Object, e As EventArgs) Handles btnExit.MouseLeave
+        btnExit.BackgroundImage = My.Resources.x_default
+    End Sub
+    Private Sub btnExitMouseDown(sender As Object, e As EventArgs) Handles btnExit.MouseDown
+        btnExit.BackgroundImage = My.Resources.x_hold
+    End Sub
+
+    Private Sub btnExitMouseUp(sender As Object, e As EventArgs) Handles btnExit.MouseUp
+        btnExit.BackgroundImage = My.Resources.x_default
+    End Sub
+#End Region
+
+#Region "Minimize Button"
+    Private Sub btnMinEnter(sender As Object, e As EventArgs) Handles btnMinimize.MouseEnter
+        sender.BackgroundImage = My.Resources.minimize_hover
+    End Sub
+    Private Sub btnMinExit(sender As Object, e As EventArgs) Handles btnMinimize.MouseLeave
+        sender.BackgroundImage = My.Resources.minimize_default
+    End Sub
+    Private Sub btnMinMouseDown(sender As Object, e As EventArgs) Handles btnMinimize.MouseDown
+        sender.BackgroundImage = My.Resources.minimize_hold
+    End Sub
+
+    Private Sub btnMinMouseUp(sender As Object, e As EventArgs) Handles btnMinimize.MouseUp
+        sender.BackgroundImage = My.Resources.minimize_default
+    End Sub
+#End Region
+
+#End Region
+
+    Private _current_player_turn As String = "X"
+    Private Property current_players_turn As String
+        Get
+            Return _current_player_turn
+        End Get
+        Set(value As String)
+            _current_player_turn = value
+        End Set
+    End Property
+
+    Private Sub switchplayer()
+        If current_players_turn = "O" Then
+            current_players_turn = "X"
+        ElseIf current_players_turn = "X" Then
+            current_players_turn = "O"
+        End If
+        lblTurn.Text = current_players_turn & "'s Turn"
+    End Sub
+
+    Function CheckCoordinatesExistInArray(arr As Control(,), targetX As Integer, targetY As Integer) As Boolean
+        For i As Integer = 0 To arr.GetUpperBound(0)
+            For j As Integer = 0 To arr.GetUpperBound(1)
+                If i = targetX AndAlso j = targetY Then
+                    Return True
+                End If
+            Next
+        Next
+
+        Return False
+    End Function
+
+    Private last_three_pics(2) As PictureBox
+    Private Function check_for_win(coords As Array) As Boolean
+        Dim type As String = cells(coords(0), coords(1)).Tag
+        Dim count As Integer
+        Dim winner As Boolean = False
+
+        Dim move_x As Integer
+        Dim move_y As Integer
+
+
+
+        Dim diagonal As Array = {coords(0) - 2, coords(1) + 2}
+        count = 0
+        move_x = 1
+        move_y = -1
+        For i As Integer = 0 To 4
+            If CheckCoordinatesExistInArray(cells, diagonal(0), diagonal(1)) AndAlso cells(diagonal(0), diagonal(1)).Tag = type Then
+                last_three_pics(count) = cells(diagonal(0), diagonal(1))
+                count += 1
+            Else
+                count = 0
+            End If
+            If count = 3 Then
+                Return True
+            End If
+            diagonal(0) += move_x
+            diagonal(1) += move_y
+        Next
+
+        Dim verticle As Array = {coords(0), coords(1) + 2}
+        count = 0
+        move_x = 0
+        move_y = -1
+        For i As Integer = 0 To 4
+            If CheckCoordinatesExistInArray(cells, verticle(0), verticle(1)) AndAlso cells(verticle(0), verticle(1)).Tag = type Then
+                last_three_pics(count) = cells(verticle(0), verticle(1))
+                count += 1
+            Else
+                count = 0
+            End If
+            If count = 3 Then
+                Return True
+            End If
+            verticle(0) += move_x
+            verticle(1) += move_y
+        Next
+
+        Dim horizontal As Array = {coords(0) - 2, coords(1)}
+        count = 0
+        move_x = 1
+        move_y = 0
+        For i As Integer = 0 To 4
+            If CheckCoordinatesExistInArray(cells, horizontal(0), horizontal(1)) AndAlso cells(horizontal(0), horizontal(1)).Tag = type Then
+                last_three_pics(count) = cells(horizontal(0), horizontal(1))
+                count += 1
+            Else
+                count = 0
+            End If
+            If count = 3 Then
+                Return True
+            End If
+            horizontal(0) += move_x
+            horizontal(1) += move_y
+        Next
+
+
+
+        Return False
+    End Function
+
+    Private Sub cell_hover(sender As PictureBox, e As EventArgs)
+        If sender.Tag = "" Then
+            If current_players_turn = "X" Then
+                sender.BackgroundImage = My.Resources.cross_hover
+            ElseIf current_players_turn = "O" Then
+                sender.BackgroundImage = My.Resources.circle_hover
+            End If
+        End If
+    End Sub
+
+    Private Sub cell_leave(sender As PictureBox, e As EventArgs)
+        If sender.Tag = "" Then
+            sender.BackgroundImage = My.Resources.empty
+        End If
+    End Sub
+    Private Sub cell_click(sender As PictureBox, e As EventArgs)
+        If sender.Tag = "" Then
+            Dim coords As Array = pic_to_coords(sender)
+            If current_players_turn = "X" Then
+                sender.Tag = "X"
+                sender.BackgroundImage = My.Resources.cross_placed
+            ElseIf current_players_turn = "O" Then
+                sender.Tag = "O"
+                sender.BackgroundImage = My.Resources.circle_placed
+            End If
+            If check_for_win(coords) Then
+                Console.WriteLine(current_players_turn & " won")
+                For Each pictureBox In last_three_pics
+                    If current_players_turn = "X" Then
+                        pictureBox.BackgroundImage = My.Resources.cross_win
+                    ElseIf current_players_turn = "O" Then
+                        pictureBox.BackgroundImage = My.Resources.circle_win
+                    End If
+
+                Next
+            Else
+                switchplayer()
+            End If
+
+        End If
+    End Sub
+
+    Private cells(2, 2) As Control
+
+    Private Function pic_to_coords(picturebox As PictureBox) As Array
+        Dim coords(2) As String
+        coords = picturebox.Name.Split(",")
+        Return coords
+    End Function
+
+    Private Sub populate_array()
+        'populates picturebox array and the board
+        For x As Integer = 0 To 2
+            For y As Integer = 0 To 2
+                Dim cell As New PictureBox
+                With cell
+                    .Name = String.Format("{0},{1}", x, y)
+                    .BackgroundImage = My.Resources.empty
+                    .AutoSize = False
+                    .Size = New Size(101, 101)
+                    .BackgroundImageLayout = ImageLayout.Stretch
+                    .Margin = New Padding(3, 3, 3, 3)
+                    .BackColor = Color.Red
+                End With
+                AddHandler cell.MouseEnter, AddressOf cell_hover
+                AddHandler cell.MouseLeave, AddressOf cell_leave
+                AddHandler cell.Click, AddressOf cell_click
+                cells(x, y) = cell
+                board.Controls.Add(cell)
+
+            Next
+        Next
+    End Sub
+
+    Private Sub Form1_Load_1(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Dim browserMenuRenderer As New BrowserMenuRenderer()
+        'SettingsToolStripMenuItem.DropDown.Renderer = browserMenuRenderer
+        lblTurn.Text = current_players_turn & "'s Turn"
+        populate_array()
+    End Sub
+End Class
